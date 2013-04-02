8d2fc6f4e963b90322066db80346f398f0fd4e9f
YANAGISAWA Chihiro <chihiroyn@gmail.com>
2013-04-03 06:57:10 +0900

    store_controllerをリファクタリング。find_cartが乱立していたのを、before_filterに集約した

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,13 +1,12 @@
 class StoreController < ApplicationController
+  before_filter :find_cart, :except => :empty_cart
 
   def index
     @products = Product.find_products_for_sale
-    @cart = find_cart
   end
   
   def add_to_cart
     product = Product.find(params[:id])
-    @cart = find_cart
     @current_item = @cart.add_product(product)
 
     respond_to do |format|
@@ -25,7 +24,6 @@ class StoreController < ApplicationController
   end
   
   def checkout
-    @cart = find_cart
     if @cart.items.empty?
       redirect_to_index("カートは現在空です")
     else
@@ -34,7 +32,6 @@ class StoreController < ApplicationController
   end
   
   def save_order
-    @cart = find_cart
     @order = Order.new(params[:order])
     @order.add_line_items_from_cart(@cart)
     
@@ -48,10 +45,7 @@ class StoreController < ApplicationController
 
 private
   def find_cart
-    unless session[:cart]
-      session[:cart] = Cart.new
-    end
-    session[:cart]
+    @cart = (session[:cart] ||= Cart.new)
   end
   
   def redirect_to_index(msg = nil)
